a739bc7230b0c3e944e0950379102729a47da2a4
DigitalOnion <luis.viruena@gmail.com>
2017-10-17 10:24:19 -0700

    Add basic form with new dependency resolution

--- app/src/main/java/com/outerspace/daggerscone/MainActivity.java
+++ app/src/main/java/com/outerspace/daggerscone/MainActivity.java
@@ -2,12 +2,35 @@ package com.outerspace.daggerscone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.outerspace.daggerscone.model.Person;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Person person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        person = new Person();
+        person.firstName = "Luis Jesús";
+        person.lastNameF = "Virueña";
+        person.lastNameM = "Silva";
+
+        EditText edit;
+        edit = (EditText) findViewById(R.id.first_name);
+        edit.setText(person.firstName);
+        edit = (EditText) findViewById(R.id.last_name_f);
+        edit.setText(person.lastNameF);
+        edit = (EditText) findViewById(R.id.last_name_m);
+        edit.setText(person.lastNameM);
+        
     }
 }
---
+++ app/src/main/java/com/outerspace/daggerscone/model/Person.java
@@ -0,0 +1,11 @@
+package com.outerspace.daggerscone.model;
+
+/**
+ * Created by user on 10/17/17.
+ */
+
+public class Person {
+    public String firstName;
+    public String lastNameF;
+    public String lastNameM;
+}
